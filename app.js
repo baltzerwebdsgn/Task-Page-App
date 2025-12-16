da16704a3bec11a462a1aee31f43f706904b087b
baltzerwebdsgn <nikki@baltzerwebdesign.com>
2025-12-16 14:57:17 -0800

    added clear notes function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-document.addEventListener("DOMContentLoaded", (event) => {});
+//Clear notes functionality
+document.addEventListener("DOMContentLoaded", (event) => {
+  const clearNotesButton = document.getElementById("clear-notes-button");
+
+  if (clearNotesButton) {
+    clearNotesButton.addEventListener("click", function (event) {
+      alert("Your notes have been cleared!");
+      document.getElementById("notes-area").value = "";
+    });
+  } else {
+    console.error("The element with the ID '' was not found in the DOM.");
+  }
+});
