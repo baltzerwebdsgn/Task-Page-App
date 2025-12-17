908a878f27ae6e7b16775deab52e20695d948b43
baltzerwebdsgn <nikki@baltzerwebdesign.com>
2025-12-17 10:02:58 -0800

    added remove button functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   //tasks progress bar functionality
   setupProgressTracker("steps", "tasks-progress-bar");
+
   //Clear notes functionality
   const clearNotesButton = document.getElementById("clear-notes-button");
 
@@ -12,6 +13,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
   } else {
     console.error("The element with the ID '' was not found in the DOM.");
   }
+  //Remove task step functionality
+  const removeButtons = document.querySelectorAll(".remove-step");
+
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const listItemToDelete = this.parentElement;
+      listItemToDelete.remove();
+      //Handles the case when the user deletes a completed task and updates the progress bar
+      setupProgressTracker("steps", "tasks-progress-bar");
+    });
+  });
 });
 const starTaskArray = new Array(3);
 const starTaskCounter = 0;
