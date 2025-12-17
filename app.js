b78f7f1b99263606b8057622064fb8edcc90c373
baltzerwebdsgn <nikki@baltzerwebdesign.com>
2025-12-16 16:56:01 -0800

    added progress bar basic functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-//Clear notes functionality
 document.addEventListener("DOMContentLoaded", (event) => {
+  //tasks progress bar functionality
+  setupProgressTracker("steps", "tasks-progress-bar");
+  //Clear notes functionality
   const clearNotesButton = document.getElementById("clear-notes-button");
 
   if (clearNotesButton) {
@@ -11,3 +13,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.error("The element with the ID '' was not found in the DOM.");
   }
 });
+const starTaskArray = new Array(3);
+const starTaskCounter = 0;
+
+function countCheckedCheckboxes(className) {
+  const selector = `.${className}:checked`;
+  return document.querySelectorAll(selector).length;
+}
+
+function countAllCheckboxes(className) {
+  return document.querySelectorAll(`.${className}`).length;
+}
+function setupProgressTracker(className, progressElementId) {
+  const allCheckboxes = document.querySelectorAll(`.${className}`);
+  allCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      updateProgress(className, progressElementId);
+    });
+  });
+  updateProgress(className, progressElementId);
+}
+function updateProgress(className, progressElementId) {
+  const totalCheckedboxes = countAllCheckboxes(className);
+  const checkedCounted = countCheckedCheckboxes(className);
+
+  const progressBar = document.getElementById(progressElementId);
+  if (progressBar) {
+    progressBar.value = checkedCounted;
+    progressBar.max = totalCheckedboxes;
+  }
+}
